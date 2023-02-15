ad8adb470547e9f919bc21b53c3102389155ab25
Jonah Saywonson <tug22941@temple.edu>
2023-02-15 16:06:50 -0500

    Feb 15 classwork

---
+++ Mod_303_7/src/SortingEx/HashMapEx.java
@@ -0,0 +1,51 @@
+package SortingEx;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapEx {
+
+	public static void main(String[] args) {
+		
+		//Map with Key Type String & Key Type Value
+		Map<String,String> dictionary = new HashMap<>();
+		dictionary.put("key", "value");
+		dictionary.put("concat", "To add 2 strings together");
+		dictionary.put("instanciate", "to create a new object");
+		dictionary.put("new","allocate new memeory location");
+		
+		//loop through the strMap Map
+		for(String key: dictionary.keySet()) {
+			System.out.println(key + " = " + dictionary.get(key));
+		}
+		System.out.println("===========================");
+		
+		//get the value of the 'concat' key from the strMap Map
+		System.out.println(dictionary.get("concat"));
+		
+		//Map with Key Type String & Value Type Integer
+		Map<String,Integer> intMap = new HashMap<>();
+		intMap.put("key",1);
+		
+		Map<Integer,String> sIntMap = new HashMap<>();
+		sIntMap.put(1, "one");
+		sIntMap.put(2, "two");
+		sIntMap.put(3, "three");
+		System.out.println(sIntMap.get(3));
+		
+		//overwrite the the value of key: 3
+		sIntMap.put(3,"THREE");
+		System.out.println(sIntMap.get(3));
+		
+		//loop through the sIntMap Map
+		for(Integer key : sIntMap.keySet() ) {
+			System.out.print(key);
+		}
+
+		
+
+		
+
+	}
+
+}
---
+++ Mod_303_7/src/ass_303_7/SortingExamples.java
@@ -0,0 +1,31 @@
+package ass_303_7;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class SortingExamples {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.add(13);
+		numbers.add(7);
+		numbers.add(18);
+		numbers.add(5);
+		numbers.add(2);
+		
+		System.out.println("Before: " + numbers);
+		
+		//Collections is a set of tools
+		//Use Static Utility method on the Collection object
+		Collections.sort(numbers);
+		System.out.println("After: " + numbers);
+		
+		//Use sort function on the array itself
+		numbers.sort(Comparator.reverseOrder());
+		
+		//....
+	}
+
+}
