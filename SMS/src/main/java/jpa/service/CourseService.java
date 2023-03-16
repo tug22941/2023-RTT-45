c6165b3709df5eaec1306a855aad1a5e46bf9872
Jonah Saywonson <tug22941@temple.edu>
2023-03-16 07:48:41 -0400

    completed student management system KBA!! Module 306.3

--- SMS/src/main/java/jpa/service/CourseService.java
+++ SMS/src/main/java/jpa/service/CourseService.java
@@ -1,5 +1,34 @@
 package jpa.service;
 
-public class CourseService {
+import java.util.List;
 
+import jpa.dao.CourseDAO;
+import jpa.entitymodels.Course;
+
+public class CourseService {
+	private CourseDAO courseDAO = new CourseDAO();
+	
+	//default constructor
+	public CourseService() {
+		
+	}
+	
+	//get a list of all courses form the database
+	public List<Course> getAllCourses(){
+		return courseDAO.getAllCourses();
+	}
+	
+	//get a single course matching the input id
+	public Course getCourseById(Integer id) {
+		
+		List<Course> allCourses = getAllCourses();
+		for(Course c: allCourses) {
+			if(c.getCId() == id) {
+				return c;
+			}
+		}
+		return null;
+	}
+	
+	
 }
--- SMS/src/main/java/jpa/service/StudentService.java
+++ SMS/src/main/java/jpa/service/StudentService.java
@@ -3,15 +3,16 @@ package jpa.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import jpa.dao.CourseDAO;
 import jpa.dao.StudentDAO;
 import jpa.entitymodels.Course;
 import jpa.entitymodels.Student;
+import jpa.entitymodels.StudentCourse;
+
+//service class for student actions
 
 public class StudentService {
 
 	private StudentDAO studentDAO = new StudentDAO();
-	private CourseDAO courseDAO = new CourseDAO();
 	private Student student;
 	
 	//default constructor
@@ -19,6 +20,7 @@ public class StudentService {
 		
 	}
 	
+	//get a list of students, containing only 1 student matching the input email
 	public List<Student> getStudentByEmail(String email) {
 		List<Student> students= new ArrayList<Student>();
 		student = studentDAO.getStudentByEmail(email);
@@ -26,7 +28,41 @@ public class StudentService {
 		return students;
 	}
 	
-	public List<Course> getStudentCourses(Integer id){
-		return studentDAO.getStudentCourses(id);
+	//get a list of courses of courses belonging to the matching studentId
+	public List<Course> getStudentCourses(Integer studentId){
+		return studentDAO.getStudentCourses(studentId);
 	}
+	
+	//validate that a student, matching the input id, is not already registered to a course from the input course list
+	public boolean canRegisterStudent(Integer id, List<Course> stdCourses) {
+		for(Course c: stdCourses) {
+			if(c.getCId() == id) {
+				return false;
+			}
+		}return true;
+	}
+	
+	//register a student to a course
+	public void registerStudentToCourse(Student student, Course course) {
+		StudentCourse studentCourse = new StudentCourse();
+		studentCourse.setSId(student.getSId());
+		studentCourse.setCId(course.getCId());
+		studentCourse.setStudent(student);
+		studentCourse.setCourse(course);
+		studentDAO.registerStudentToCourse(studentCourse);
+	}
+	
+	//-- Not Used
+	
+	//get a list of all students form the database
+	public List<Student> getAllStudents(){
+		return studentDAO.getAllStudents();
+	}
+	
+	//validate whether a student's email and password pair is present in the database
+	public boolean validateStudent(String email, String password) {
+		return studentDAO.validateStudent(email, password);
+	}
+	
+	
 }
