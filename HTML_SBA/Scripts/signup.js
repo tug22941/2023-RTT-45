81ed235073b4a1e2c3fde193bb6d8ccc1550d6cf
Jonah Saywonson <tug22941@temple.edu>
2023-03-23 10:08:40 -0400

    started HTML SBA. Continued to do claswork on module 308.4

---
+++ HTML_SBA/Scripts/signup.js
@@ -0,0 +1,13 @@
+let firstName = document.getElementById("txtFirstName");
+let lastName = document.getElementById("txtLastName");
+let email = document.getElementById("txtEmail");
+let password = document.getElementById("txtPassowrd");
+let confirmPassword = document.getElementById("txtConfirmPassword");
+let phone = document.getElementById("txtPhoneNumber");
+let company = document.getElementById("txtCompany");
+let address1 = document.getElementById("txtAddressLine1");
+let address2 = document.getElementById("txtAddressLine2");
+let country = document.getElementById("ddlCountry");
+let state = document.getElementById("txtState");
+
+console.log(firstName.value);
