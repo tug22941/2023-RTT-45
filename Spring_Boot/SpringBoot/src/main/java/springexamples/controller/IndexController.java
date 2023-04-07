2ff8af3b9fc73ef05a7bc71d0f790cd0a39ad580
Jonah Saywonson <tug22941@temple.edu>
2023-04-07 16:18:15 -0400

    Started Exercise on Spring Security

--- Spring_Boot/SpringBoot/src/main/java/springexamples/controller/IndexController.java
+++ Spring_Boot/SpringBoot/src/main/java/springexamples/controller/IndexController.java
@@ -2,17 +2,32 @@ package springexamples.controller;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import springexamples.database.dao.UserDAO;
+import springexamples.database.dao.UserRoleDAO;
+import springexamples.database.entity.User;
+import springexamples.database.entity.UserRole;
 import springexamples.formbeans.CreateUserFormBean;
 
 @Slf4j
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRoleDAO userRoleDAO;
 
     //make first message to handle incoming request
     @RequestMapping(value = {"/index","/", "/index.html"}, method = RequestMethod.GET)
@@ -37,20 +52,46 @@ public class IndexController {
         log.debug("In the signup GET controller method");
         ModelAndView response = new ModelAndView("signup");
 
-        session.setAttribute("name", "Session set on Signup");
+        session.setAttribute("name", "Session set on Singup");
         return response;
     }
-
     @PostMapping("/signup")
     public ModelAndView signup(CreateUserFormBean form) {
 
         ModelAndView response = new ModelAndView("signup");
         log.debug("In the signup POST controller method");
-
         log.debug(form.toString());
+
+        User user = new User();
+        user.setEmail(form.getEmail());
+        user.setFullName(form.getFullName());
+
+        //This step is required by spring security to encrypt password on creation
+        String encryptedPassword = passwordEncoder.encode(form.getPassword());
+        user.setPassword(encryptedPassword);
+        //save the user: when hibernate saves user it will auto generate the ID
+        // -- AND it will populate the field in the entity
+        userDAO.save(user); //SAVED IN DATABASE
+
+        //create user role object
+        UserRole userRole = new UserRole();
+        userRole.setRoleName("USER");
+        userRole.setUserId(user.getId());
+        //user entity has an id from previous 'userDAO.save' database population
+        userRoleDAO.save(userRole);
 
         return response;
     }
+
+    @GetMapping(value = "/login")
+    public ModelAndView login(HttpSession session) {
+        log.debug("In the login GET controller method");
+        ModelAndView response = new ModelAndView("login/login");
+
+        session.setAttribute("name", "Session set on Login");
+        return response;
+    }
+
 
     @RequestMapping(value = "/bootstrap", method = RequestMethod.GET)
     public ModelAndView bootstrap(HttpSession session) {
