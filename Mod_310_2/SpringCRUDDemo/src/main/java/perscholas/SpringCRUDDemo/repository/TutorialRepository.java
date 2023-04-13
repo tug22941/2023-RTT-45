13aefec3f85d9f269e1e53ef338067ff68225f1b
Jonah Saywonson <tug22941@temple.edu>
2023-04-13 15:48:25 -0400

    Refactored module folder containing Spring CRUD demos Demo 1: with controller and service package in Mod 310.1 Demo 2: ENHANCED - Spring Data Rest in place of controller and service classes in Mod 310.2

--- Mod_310_2/SpringCRUDDemo/src/main/java/perscholas/SpringCRUDDemo/repository/TutorialRepository.java
+++ Mod_310_2/SpringCRUDDemo/src/main/java/perscholas/SpringCRUDDemo/repository/TutorialRepository.java
@@ -1,12 +1,13 @@
 package perscholas.SpringCRUDDemo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import perscholas.SpringCRUDDemo.model.Tutorial;
 
 import java.util.List;
 import java.util.Optional;
-@Repository
+@RepositoryRestResource(path = "learningtutorials")
 public interface TutorialRepository extends JpaRepository<Tutorial, Long>
 // Tutorial → Entity type and Long --> primary key
 {
